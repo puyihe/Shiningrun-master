f4139d4f7a1ec0dc78d26d9022e18a9d412965c1
puyihe <82323222+puyihe@users.noreply.github.com>
2023-06-11 13:13:08 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
+// 
+//Here's Johnny:
+//https://www.myinstants.com/en/instant/heres-johnny/
+//https://movie-sounds.org/horror-movie-samples/quotes-with-sound-clips-from-the-shining-1980/come-and-play-with-us-danny-forever-and-ever-and-ever
+//assets：
+//https://kamisama887.itch.io/lorenz-fries-school-horror
+//https://shubibubi.itch.io/cozy-people
+//scenes3
+//https://ninjikin.itch.io/grass
+//backgroundmusic：
+//Halloween Horror Creepy Piano | Horror by Alex-Productions | https://onsound.eu/
+//Music promoted by https://www.chosic.com/free-music/all/
+//Creative Commons CC BY 3.0
+//https://creativecommons.org/licenses/by/3.0/
+//Come Play with Me Kevin MacLeod (incompetech.com)
+//Licensed under Creative Commons: By Attribution 3.0 License
+//http://creativecommons.org/licenses/by/3.0/
+//Music promoted by https://www.chosic.com/free-music/all/
+//Demented Nightmare by Darren Curtis | https://www.darrencurtismusic.com/
+//Music promoted by https://www.chosic.com/free-music/all/
+//Creative Commons CC BY 3.0
+//https://creativecommons.org/licenses/by/3.0/
+//
 let config = {
     type: Phaser.CANVAS,
     render: {
